1104b43bf24b9cc64fdabf9ea0cead9827576ed8
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-05-07 07:48:27 +0300

    Update API URL in 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -18,7 +18,7 @@ $(document).ready(function () {
 
   $.ajax({
     type: 'GET',
-    url: 'http://172.27.3.156:5001/api/v1/status/',
+    url: 'http://0.0.0.0:5001/api/v1/status/',
     dataType: 'json',
     success: function (response, status) {
       if (status === 'success' && response.status === 'OK') {
