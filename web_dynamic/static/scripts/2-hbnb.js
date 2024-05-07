f051bec8b4e75e548667e6d3a2c946ce13f5462f
Abdelrahman Mohamed Elsayed <3bdelrahmanm0o@gmail.com>
2024-05-07 06:19:31 +0300

    Implement checkbox change event handling in 2-hbnb.js

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,21 @@
 $(document).ready(function () {
+  let checkedElement = [];
+  $('input[type="checkbox"]').change(function () {
+    const amenityId = $(this).attr('data-id');
+    const amenityName = $(this).attr('data-name');
+    if ($(this).is(':checked')) {
+      const amenitySpan = $('<span>', { 'amenity-id': amenityId });
+      $(amenitySpan).text(amenityName);
+      checkedElement.push(amenitySpan[0]);
+    } else {
+      checkedElement = checkedElement.filter(amenity => $(amenity).attr('amenity-id') !== amenityId);
+    }
+    const joinedSpans = checkedElement.map(span => span.outerHTML).join(', ');
+    $('section.filters div.amenities h4').html(`&nbsp;${joinedSpans}`);
+    // console.log($(this).is(':checked') ? 'appended' : 'removed');
+    // console.log(joinedSpans);
+  });
+
   $.ajax({
     type: 'GET',
     url: 'http://172.27.3.156:5001/api/v1/status/',
